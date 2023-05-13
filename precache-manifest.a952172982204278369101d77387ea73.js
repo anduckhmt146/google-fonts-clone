8c335ebc3e6db7e190387d807a13320029bfc63f
Nguyen Duc An <an.nguyenduc1406@hcmut.edu.vn>
2023-05-13 12:26:38 +0700

    Updates

---
+++ precache-manifest.a952172982204278369101d77387ea73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68bba4ed5bd16ad7559241fc3dbb565c",
+    "url": "/google-fonts-clone/index.html"
+  },
+  {
+    "revision": "3699b6548ff3ce2c590c",
+    "url": "/google-fonts-clone/static/css/main.1defd218.chunk.css"
+  },
+  {
+    "revision": "b7bdb0e5b60385b71b59",
+    "url": "/google-fonts-clone/static/js/2.659e05a9.chunk.js"
+  },
+  {
+    "revision": "3699b6548ff3ce2c590c",
+    "url": "/google-fonts-clone/static/js/main.83922e4d.chunk.js"
+  },
+  {
+    "revision": "b2089b8588007f4844b1",
+    "url": "/google-fonts-clone/static/js/runtime-main.cbb0c08a.js"
+  }
+]);
